5f6ad5620b9f6587dee30ee921bf6667272c4605
Александр <alexander.bozhko@nordclan.com>
2025-06-09 16:51:14 +0400

    добавил настройки

--- src/main/java/ru/cloudstorage/storage/configurations/OpenApiConfig.java
+++ src/main/java/ru/cloudstorage/storage/configurations/OpenApiConfig.java
@@ -1,11 +1,7 @@
 package ru.cloudstorage.storage.configurations;
 
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.info.Info;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import org.springdoc.core.GroupedOpenApi;
-//import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 /**
  * @autor Bozhko Alexander
@@ -13,11 +9,11 @@ import org.springframework.context.annotation.Configuration;
  * Конфигурация для OpenApi
  */
 @Configuration
-//@OpenAPIDefinition(
-//        info = @Info(
-//                title = "Cloud Storage API",
-//                version = "1.0.0"
-//        )
-//)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cloud Storage API",
+                version = "1.0.0"
+        )
+)
 public class OpenApiConfig {
 }
